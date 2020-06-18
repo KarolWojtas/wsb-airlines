e5ccd94b4b3d84880b04e5508384a8fd3de76c8f
Karol Wojtas <karolw@lsnova.pl>
2020-06-18 19:36:23 +0200

    started services

--- src/containers/Services.js
+++ src/containers/Services.js
@@ -1,7 +1,43 @@
-import React from "react";
+import React, { useMemo } from "react";
+import parking from '../assets/underground-parking.jpg'
+import airport from '../assets/airport.jpg';
+import hotel from '../assets/hotel.jpg'
 
 const Services = () => {
-  return <div>services</div>;
+  const services = useMemo(() => {
+    return [
+      {
+        title: 'Parking',
+        img: parking
+      },
+      {
+        title: 'Pierwszeństwo wsiadania',
+        img: airport
+      },
+      {
+        title: 'Hotel',
+        img: hotel
+      }
+    ]
+  }, []);
+  return <div className="container">
+    <div className="row my-4">
+      {services.map(service => <div className="col-md-4">
+        <div className="card">
+          <svg class="card-img-top"
+            viewBox="0 0 100 100"
+            style={{ width: '100%', backgroundImage: `url(${service.img})`, backgroundSize: 'cover', backgroundRepeat: 'no-repeat', backgroundPositionX: 'center' }}>
+            <path d="M 0,100 L 100,80 V100 H0" fill="white"></path>
+          </svg>
+
+          <div className="card-body bg-gradient-light">
+            <h5 className="card-title">{service.title}</h5>
+            <p className="card-text">text</p>
+          </div>
+        </div>
+      </div>)}
+    </div>
+  </div>;
 };
 
 export default Services;
